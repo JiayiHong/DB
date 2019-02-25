6b5ca17b3a622ef53e7f2f1a37bcb35e8ca2ffe3
hongjiayi <hjy.40hjy@163.com>
2019-02-25 14:29:04 +0000

    create Table class

---
+++ Table.java
@@ -0,0 +1,22 @@
+
+class Table {
+
+	private String name;
+	
+
+	Table(String name) {
+		this.name = name;
+	}
+
+
+
+
+
+
+
+//	===============TESTING===============
+
+	public static void main(String[] args) {
+		Table program = new Table();
+	}
+}
